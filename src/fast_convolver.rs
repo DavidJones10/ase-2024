013fd22a9e7ca4b8355494ef47ceeea9f54f3be7
DavidJones10 <102177843+DavidJones10@users.noreply.github.com>
2024-04-24 00:42:43 -0400

    Added comments

--- src/fast_convolver.rs
+++ src/fast_convolver.rs
@@ -6,7 +6,7 @@ use crate::ring_buffer::RingBuffer;
 use realfft::{ComplexToReal, RealFftPlanner, RealToComplex};
 use rustfft::{num_complex::{Complex, Complex32}, num_traits::PrimInt, Fft, FftPlanner};
 
-
+/// A time-domain and frequency-domain convolver
 pub struct FastConvolver {
     ir: Vec<f32>,
     tail: RingBuffer<f32>,
@@ -19,12 +19,14 @@ pub struct FastConvolver {
 
 #[derive(Debug, Clone, Copy)]
 pub enum ConvolutionMode {
-    TimeDomain,
+    TimeDomain, 
     FrequencyDomain { block_size: usize },
 }
 
 impl FastConvolver {
-    // Block Size will be defined as the size of the blocks of input
+    /// Creates a new FastConvolver Object.
+    /// 
+    /// Block Size for FrequencyDomain should be the size of the slices into the proces function
     pub fn new(impulse_response: &[f32], mode: ConvolutionMode) -> Self {
         let buffer = RingBuffer::<f32>::new(impulse_response.len()-1);
         let mut fft = None;
@@ -53,11 +55,12 @@ impl FastConvolver {
         }
 
     }
-
+    /// Takes the impulse response, splits it into chunks, takes an fft of each chunk,
+    /// and returns a vec of those fft chunks
     fn bake_ir(impulse_response: &[f32], block_size: usize)-> Vec<Vec<Complex32>>{
         let mut bake = vec![vec![]];
         let mut chunks_vec= vec![vec![0.0]];
-        let mut chunks = impulse_response.chunks(block_size);
+        let chunks = impulse_response.chunks(block_size);
         chunks.clone().for_each(|chunk|{chunks_vec.push(chunk.to_vec())});
         
         let mut plan = FftPlanner::<f32>::new();
@@ -75,11 +78,12 @@ impl FastConvolver {
         //println!("before everything: {:?}", bake[1..bake.len()].to_vec());
         bake[1..bake.len()].to_vec()
     }
-    
+    /// Clears the overlap/tail buffer
     pub fn reset(&mut self) {
         self.tail.reset();
     }
-
+    /// Computes the convolution between the input slice and the impulse
+    /// response and copys the result to the output slice
     pub fn process(&mut self, input: &[f32], output: &mut [f32]) {
         match self.conv_mode{
             ConvolutionMode::TimeDomain =>{self.conv_time_domain(input, output);},
@@ -88,18 +92,21 @@ impl FastConvolver {
             }
         }
     }
-
+    /// Copys the tail of the impulse response to the output
+    /// 
+    /// This should be called after the last process call
     pub fn flush(&mut self, output: &mut [f32]) {
         // Flushes remaining samples into the output
         for out_samp in output.iter_mut(){ //0..self.ir.len()-1
             *out_samp = self.tail.pop();
         }
     }
-
+    /// Returns the necessary size of an output buffer given the size of 
+    /// the input buffer
     pub fn get_output_size(&mut self, input_size: usize)->usize{
         input_size + self.ir.len() -1
     }
-
+    /// Computes the convolution in the time domain
     fn conv_time_domain(&mut self, input: &[f32], output: &mut [f32]){
         let mut complete_buffer = vec![0.0;input.len()+self.ir.len() -1];
         for i in 0..input.len(){
@@ -116,7 +123,7 @@ impl FastConvolver {
             self.tail.push(complete_buffer[m]);
         }
     }
-
+    /// Computes the convolution in the frequency domain
     fn conv_freq_domain(&mut self, input: &[f32], output: &mut [f32], block_size: usize) {
         assert!(input.len() <= block_size);
         let mut total_conv = vec![vec![Complex::<f32>::new(0.0, 0.0);self.fft_length.unwrap()];self.baked_ir.as_ref().unwrap().len()];
@@ -144,19 +151,6 @@ impl FastConvolver {
         }
     }
 
-}
-
-fn next_pow_2(mut val: usize) -> usize {
-    if val == 0{
-        return 2
-    }
-    val -= 1;
-    val |= val >> 1;
-    val |= val >> 2;
-    val |= val >> 4;
-    val |= val >> 8;
-    val |= val >> 16;
-    val + 1
 }
 
 #[cfg(test)]
@@ -302,8 +296,11 @@ mod tests {
             let input_chunks = input.chunks(block_size);
             let mut output_chunks = output[0..input.len()].chunks_mut(block_size);
             for (in_chunk,out_chunk) in zip(input_chunks, output_chunks){
-                //println!("ChunkLen {}", in_chunk.len());
-                conv.process(in_chunk, out_chunk);
+                if in_chunk.len() < block_size{
+                    
+                }else{
+                    conv.process(in_chunk, out_chunk);
+                }
             }
             conv.flush(&mut output[input.len()..]);
                 for i in 0..output.len(){
@@ -314,65 +311,4 @@ mod tests {
                 }
         }
     }
-
-    #[test]
-    fn testPow2(){
-        let i = 25;
-        let j = 1020;
-        let k = 0;
-        let l = 1025;
-        assert_eq!(next_pow_2(i),32);
-        assert_eq!(next_pow_2(j),1024);
-        assert_eq!(next_pow_2(k), 2);
-        assert_eq!(next_pow_2(l),2048);
-    }
 }
-
-//cargo flamegraph -- <parameters>
-//google-chrome flamegraph.svg
-// cargo flamegraph -f -- <parameters>  //adds a lot more samples
-// cargo run 
-
-// godbolt.org
-
-// use simd
-
-
-/* 
-fn conv_freq_domain(&mut self, input: &[f32],output: &mut [f32],block_size: usize){ 
-    if self.ir.len() <= block_size{
-        let mut ir_copy = vec![0.0;self.fft_length.unwrap()];
-        let mut input_copy = vec![0.0;self.fft_length.unwrap()];
-        ir_copy[..self.ir.len()].copy_from_slice(&self.ir);
-        input_copy[..input.len()].copy_from_slice(&input);
-        let mut ir_comp = self.fft.as_mut().unwrap().make_output_vec();
-        let mut in_comp = self.fft.as_mut().unwrap().make_output_vec();
-        self.fft.as_mut().unwrap().process(&mut ir_copy, &mut ir_comp);
-        self.fft.as_mut().unwrap().process(&mut input_copy, &mut in_comp);
-        for i in 0..ir_comp.len(){
-             in_comp[i] *= ir_comp[i]
-        }
-        let mut ifft_buff = self.ifft.as_mut().unwrap().make_output_vec();
-        self.ifft.as_mut().unwrap().process(&mut in_comp, &mut ifft_buff);
-        for i in 0..output.len(){
-            output[i] = 0.0;
-        }
-
-    }else{
-
-    }
-    for i in 0..block_size{
-
-    }
-    /* let mut ir_copy = vec![0.0;block_size];
-    let mut input_copy = vec![0.0;block_size];
-    let mut output_samples = real.make_output_vec();
-    let mut output_samples_ir = real.make_output_vec();
-    input_copy[..block_size.min(input.len())].copy_from_slice(&input[..block_size.min(input.len())]);
-    ir_copy[..block_size.min(self.ir.len())].copy_from_slice(&self.ir[..block_size.min(self.ir.len())]);
-
-    for i in 0..output_samples.len(){
-        output_samples[i] *= output_samples_ir[i];
-    } */
-    
-} */
